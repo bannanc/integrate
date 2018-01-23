8eb1123004b5bdbab1e2e50011b40711138f8c8c
bannanc <bannanc@uci.edu>
2018-01-23 15:04:00 -0500

    add documentation for simpson

--- integrate/newton_cotes/simpson.py
+++ integrate/newton_cotes/simpson.py
@@ -5,11 +5,27 @@ This file contains the implementation of the Simpson rule
 import numpy as np
 
 
-def evaluate(x, f):
-    a = float(x[0])
-    b = float(x[1])
-    ya = f(a)
-    yb = f((a + b) / 2)
-    yc = f(b)
+def evaluate(bounds, func):
+    """
+    Evaluates simpsons rules on an array of values and a function pointer
+
+    Parameters
+    ----------
+    bounds: array_like
+        An array with a dimension of two that contains the starting and
+        ending points for the integrand.
+    func: function
+        A function being evaluted in this integral
+
+    Returns
+    -------
+    integral: float
+        The integral of function (func) between the bounds
+    """
+    a = float(bounds[0])
+    b = funcloat(bounds[1])
+    ya = func(a)
+    yb = func((a + b) / 2)
+    yc = func(b)
     I = (b - a) * (ya + 4 * yb + yc) / 6.0
     return I
