6a89b2ab6802b6c591164b9de829ea1d4969b38b
bannanc <bannanc@uci.edu>
2018-01-23 13:47:00 -0500

    fix all instances of integer division for py2 compatibility

--- integrate/stochastic/monte_carlo.py
+++ integrate/stochastic/monte_carlo.py
@@ -9,12 +9,12 @@ def monte_1d(x, f, trials):
    b = x[1]
    d = (b - a) *  np.random.rand(1, trials) + a
    y = f(d)
-   return (b-a) * np.sum(y) / trials
+   return (b-a) * np.sum(y) / float(trials)
 
 def monte_2d(f, v, domain, trials):
    area = np.prod(domain[1] - domain[0])
    matrix = np.sqrt(area) * np.random.rand(trials, 2) + np.min(domain)
-   v_eval = v(matrix) 
+   v_eval = v(matrix)
    idx = np.where(v_eval < 1.0)
    points_inside = v_eval[idx]
    k = points_inside.size
