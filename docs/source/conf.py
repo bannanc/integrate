4fba756270847b2b4c66b45277468577a0c53354
bannanc <bannanc@uci.edu>
2018-02-01 16:41:18 -0800

    make 'html_sidebars' section identical to DGAS

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -127,8 +127,11 @@ html_static_path = ['_static']
 # refs: http://alabaster.readthedocs.io/en/latest/installation.html#sidebars
 html_sidebars = {
     '**': [
+        'about.html', # missing, copied from DGASmith
+        'navigation.html', # missing, copied from DGASmith
         'relations.html',  # needs 'show_related': True theme option to display
         'searchbox.html',
+        'donate.html', # missing, copied from DGASmith
     ]
 }
 
